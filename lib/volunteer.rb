798c1e1eb129de2e2a8f7c0cc491ca24fc36fb41
LiamPKenna <liampkenna@gmail.com>
2020-01-10 09:23:41 -0800

    Add initializer and reader methods to pass the first 2 tests

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,4 +1,10 @@
 class Volunteer
-  def initialize
+
+  attr_reader :name, :id, :project_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
   end
 end
