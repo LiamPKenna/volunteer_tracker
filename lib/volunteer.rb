a4464fafad48ed52ce6b5c5cdfd6eb0dc1737bd1
LiamPKenna <liampkenna@gmail.com>
2020-01-10 09:29:21 -0800

    Add .get_volunteers and .all methods matching Project

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -12,4 +12,24 @@ class Volunteer
     self.name == volunteer_to_compare.name
   end
 
+  def self.get_volunteers(db_query)
+    returned_volunteers = DB.exec(db_query)
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch('name')
+      project_id = volunteer.fetch('project_id').to_i
+      id = volunteer.fetch('id').to_i
+      volunteers.push(Project.new({
+        :name => name,
+        :project_id => project_id,
+        :id => id
+      }))
+    end
+    volunteers
+  end
+
+  def self.all
+    self.get_volunteers('SELECT * FROM volunteers;')
+  end
+
 end
