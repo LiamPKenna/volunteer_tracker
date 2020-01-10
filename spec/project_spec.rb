91de8f8a9723216146317525ee3de400cd5aa0ca
LiamPKenna <liampkenna@gmail.com>
2020-01-10 12:42:59 -0800

    Add test for #get_hours method

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -90,4 +90,18 @@ describe Project do
       expect(Project.all).to eq []
     end
   end
+
+  context '#get_hours' do
+    it 'returns the total hours of all volunteers of a project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      volunteer1 = Volunteer.new({:name => 'Jasmine', :project_id => project.id, :id => nil, :hours => 0})
+      volunteer1.save
+      volunteer1.add_hours(2)
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project.id, :id => nil, :hours => 0})
+      volunteer2.save
+      volunteer2.add_hours(3)
+      expect(project.get_hours).to eq 5
+    end
+  end
 end
