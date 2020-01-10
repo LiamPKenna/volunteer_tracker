d4e34a1b00eb6606f0d0537170a496d3a3e1bb2a
LiamPKenna <liampkenna@gmail.com>
2020-01-10 09:57:07 -0800

    Add Project#update method

--- lib/project.rb
+++ lib/project.rb
@@ -38,4 +38,9 @@ class Project
   def volunteers
     Volunteer.find_by_project(@id)
   end
+
+  def update(attributes)
+    @title = attributes.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
 end
