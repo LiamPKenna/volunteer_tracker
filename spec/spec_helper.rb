81e1a8d292553abf7932f8d74b21273f8d639bd4
LiamPKenna <liampkenna@gmail.com>
2020-01-10 08:26:00 -0800

    Add connection to config file for Windows

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,13 @@ require "project"
 require "rspec"
 require "pry"
 require "pg"
+require "./config"
 
-DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+# FOR WINDOWS
+DB = PG.connect(TEST_DB_PARAMS)
+
+# FOR MAC
+# DB = PG.connect({:dbname => 'volunteer_tracker_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
