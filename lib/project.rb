826c5a015e2e579f037e941542d4dc32d7acd472
LiamPKenna <liampkenna@gmail.com>
2020-01-10 09:14:51 -0800

    Add .get_projects & .all methods to allow for retreval

--- lib/project.rb
+++ lib/project.rb
@@ -15,4 +15,19 @@ class Project
   def ==(project_to_compare)
     self.title == project_to_compare.title
   end
+
+  def self.get_projects(db_query)
+    returned_projects = DB.exec(db_query)
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch('title')
+      id = project.fetch('id').to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def self.all
+    self.get_projects('SELECT * FROM projects;')
+  end
 end
